bce002f02c96b2757d8d84bae44ab8a6f856d9e3
JonWLS <jwon0100@student.monash.edu>
2022-10-14 02:09:31 +0800

    first commit

---
+++ DataVis2.js
@@ -0,0 +1,9 @@
+var vis1 = "js/global_air_pollution.vg.json"
+var vis2 = "js/mismanaged_plastic_waste.vg.json"
+var vis3 = "js/change_in_mean_sea_levels.vg.json"
+var vis4 = "js/number_of_natural_disaster_events.vg.json"
+
+vegaEmbed("#vis1", vis1).then(function(result) {}).catch(console.error);
+vegaEmbed("#vis2", vis2).then(function(result) {}).catch(console.error);
+vegaEmbed("#vis3", vis3).then(function(result) {}).catch(console.error);
+vegaEmbed("#vis4", vis4).then(function(result) {}).catch(console.error);
