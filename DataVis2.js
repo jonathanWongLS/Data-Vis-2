129a7c3003d51b456c71991ee3aace3225d8e7c9
JonWLS <jwon0100@student.monash.edu>
2022-10-17 17:31:06 +0800

    Added bar chart and donut charts for global number of vehicles by fuel type

--- DataVis2.js
+++ DataVis2.js
@@ -3,9 +3,11 @@ var vis2 = "js/mismanaged_plastic_waste.vg.json"
 var vis3 = "js/change_in_mean_sea_levels.vg.json"
 var vis4 = "js/number_of_natural_disaster_events.vg.json"
 var vis5 = "js/annual_anomalies_global_avg_surface_temp.vg.json"
+var vis6 = "js/fuel_types_vehicles.vg.json"
 
 vegaEmbed("#vis1", vis1).then(function(result) {}).catch(console.error);
 vegaEmbed("#vis2", vis2).then(function(result) {}).catch(console.error);
 vegaEmbed("#vis3", vis3).then(function(result) {}).catch(console.error);
 vegaEmbed("#vis4", vis4).then(function(result) {}).catch(console.error);
 vegaEmbed("#vis5", vis5).then(function(result) {}).catch(console.error);
+vegaEmbed("#vis6", vis6).then(function(result) {}).catch(console.error);
